70e6450e0d07cab5256d8bacf0a83db43a4fb36b
kowalczp <pat.kowalczyk@wp.pl>
2020-03-31 20:41:40 +0200

    homework_3

---
+++ HomeWork/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrimeNum();
+            Console.ReadKey();
+
+        }
+
+        static void PrimeNum()
+        {
+
+
+            while (true)
+                {
+                    Console.Write("Please enter en integer: ");
+                    String number = Convert.ToString(Console.ReadLine());
+                   
+
+                if (Regex.IsMatch(number, @"^\d+$") == false)    
+                    {
+                        Console.WriteLine("Wrong data type");                        
+                    }
+                    else
+                    {
+
+                    int numberIfTrue = int.Parse(number);
+                    bool ifPrime = true;
+
+                    for (int i = numberIfTrue - 1; i > 1; i--)
+                    {
+                        if ((numberIfTrue % i) == 0)
+                        {
+                            ifPrime = false;
+                            break;
+                        }
+                    }
+                    
+                    Console.WriteLine("Your number {0} is " + ((ifPrime && numberIfTrue > 1) ? "a prime value"  : "not a prime value"), numberIfTrue);
+                    break;
+
+
+                }
+                }
+
+
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetlaWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int licznik = 10;
+            while (licznik > 0)
+            {
+                Console.WriteLine("Licznik=" + licznik);
+                licznik--;
+            }
+
+            Console.WriteLine();
+
+            licznik = 10;
+            do
+            {
+                Console.WriteLine("Licznik (do while) = " + licznik);
+                licznik--;
+            } while (licznik > 0);
+
+            Console.WriteLine();
+
+
+
+
+            while (true)
+            {
+                Console.Write("Podaj liczbę >100 i podzielna przez 2: ");
+                int liczba = Convert.ToInt32(Console.ReadLine());
+                if (liczba > 100 && liczba % 2 == 0)
+                {
+                    Console.WriteLine("poprawna liczba");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("nieporawna liczba");
+                }
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
