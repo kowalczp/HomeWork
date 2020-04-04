78835bc606ca0304b4caba407cc60bfa01c7f8d2
kowalczp <pat.kowalczyk@wp.pl>
2020-04-04 15:05:28 +0200

    Add HomeWork_4

---
+++ HomeWork_4/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_4
+{
+    class Employee
+    {
+
+        public Employee(string imie, string nazwisko, double  stawkaGodzinowa)
+        {
+             
+            Console.WriteLine("Pracownik {0} {1}, stawka godzinowa {2} zł/h", imie, nazwisko, stawkaGodzinowa);
+            Console.Write("Podaj ilość przepracowanych godzin: ");
+            Double number = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Do zapłaty " + ((number > 8) ? 8 * stawkaGodzinowa + (number - 8) * stawkaGodzinowa * 2 : (number * stawkaGodzinowa)) + " zł");
+            // break;
+
+        } 
+        
+    }
+ 
+     
+}
---
+++ HomeWork_4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee pracownik = new Employee("Anna","Kowalska",100);
+            Console.ReadKey();
+           
+        }
+        
+
+
+    }
+    
+}
